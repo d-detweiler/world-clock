0e0cee1670a4cbc16fc024ea34aefa8756673404
d-detweiler <d.s.detweiler@gmail.com>
2024-10-27 16:13:41 -0700

    Fix JavaScript

--- src/script.js
+++ src/script.js
@@ -33,14 +33,14 @@ function updateCity(event) {
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
   <div class="city">
-          <div>
-            <h2>${cityName}</h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )} <small>${cityTime.format("A")}<small></div>
-        </div>
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
   `;
 }
 
